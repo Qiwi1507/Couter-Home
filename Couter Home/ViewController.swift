5438d7775e032014a136bbd8bcca555494005e74
Qiwi1507 <125343331+Qiwi1507@users.noreply.github.com>
2023-02-16 21:19:11 +0300

    добавить в GitHub

--- Couter Home/ViewController.swift
+++ Couter Home/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lableImage: UILabel!
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBAction func ButtonCounter(_ sender: Any) {
+        count = count + 1
+        lableImage.text = "\(count)"
+        }
+    }
+    
+    
 
